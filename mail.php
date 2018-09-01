841dc5b66ea4162a5be8fb2c1192c15724ca9eec
DmitriyPoga <35847269+DmitriyPoga@users.noreply.github.com>
2018-09-01 14:40:36 +0300

    lp

---
+++ mail.php
@@ -0,0 +1,10 @@
+<?php
+$recepient = "dmitri.dwp@gmail.com";
+$sitename = "";
+
+$text = trim($_POST["text"]);
+$mail = trim($_POST["mail"]);
+$message = "E-mail: $mail \nТекст: $text";
+
+$pagetitle = "Новое сообщение с сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
